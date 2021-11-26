de7b0862fa3345a504ed4891e635f5916bca369d
Ganesh <90024574+Gbirla@users.noreply.github.com>
2021-11-26 13:10:26 +0530

    Create JS_Objects.js

---
+++ JS_Objects.js
@@ -0,0 +1,21 @@
+var person1=
+{
+    firstname:"GANESH",
+    lastname:"BIRLA",
+    officeAddress:"KHARGONE , (M.P.)"
+};
+var person2=
+{
+    firstname:"PAWAN",
+    lastname:"PATIDAR",
+    officeAddress:"KHARGONE , (M.P.)"
+};
+var person3=
+{
+    firstname:"SOM",
+    lastname:"ATRE",
+    officeAddress:"KHARGONE , (M.P)"
+};
+console.log(person1);
+console.log(person2);
+console.log(person3);
